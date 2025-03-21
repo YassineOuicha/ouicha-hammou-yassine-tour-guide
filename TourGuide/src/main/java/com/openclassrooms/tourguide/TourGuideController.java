08f95f0ca10439ea527fceb5f2abb07a4a08667a
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-03-21 05:41:46 +0100

    Installing libs manually

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
@@ -6,14 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
-
 import com.openclassrooms.tourguide.service.TourGuideService;
 import com.openclassrooms.tourguide.user.User;
 import com.openclassrooms.tourguide.user.UserReward;
-
 import tripPricer.Provider;
 
 @RestController
