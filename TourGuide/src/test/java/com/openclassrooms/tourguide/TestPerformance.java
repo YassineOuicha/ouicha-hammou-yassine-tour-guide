e543b4a4cff63ffdcdbe7283f6ae110fe9b6e33c
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-03-26 09:45:29 +0100

    Test CI/CD on dev branch

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -41,7 +41,7 @@ public class TestPerformance {
 
 		// Users should be incremented up to 100000, and test finishes within 15 minutes maximum
 
-		InternalTestHelper.setInternalUserNumber(100);
+		InternalTestHelper.setInternalUserNumber(1000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
 		List<User> allUsers = tourGuideService.getAllUsers();
